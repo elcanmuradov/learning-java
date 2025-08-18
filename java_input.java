a39ced87513afefa8841d1eb8786fb1dafc1c085
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-18 20:02:08 +0400

    4

---
+++ java_input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class java_input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        
+        scanner.nextLine();
+        System.out.println("Enter your favourite animal: ");
+        String animal = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Your age is " + age + ", Your fav animal is " + animal + "!");
+        scanner.close();
+    }
+}
